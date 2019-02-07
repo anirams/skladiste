8a7112a9c0d6376bfad0677cdaf9dae049e97be7
marino2009 <45363000+marino2009@users.noreply.github.com>
2019-02-07 11:10:46 +0100

    34tt54g

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin
-from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
-		if app.config['ELASTICSEARCH_URL'] else None
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
