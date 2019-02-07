b8967e166590dc6f5e577bca193d1e699ace79d4
Uglaos <42736734+Uglaos@users.noreply.github.com>
2019-02-07 11:10:41 +0100

    Front end

---
+++ migrations/versions/dba08509e757_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: dba08509e757
+Revises: e6b2f4a140a3, ee1ab2d995e3
+Create Date: 2019-02-07 11:06:27.688991
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dba08509e757'
+down_revision = ('e6b2f4a140a3', 'ee1ab2d995e3')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
