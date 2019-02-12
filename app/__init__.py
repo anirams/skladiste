affaf89f03d752a62fb4975ed07bd105c91b8afb
anirams <marina.stvoric1@gmail.com>
2019-02-12 11:04:02 +0100

    c
    f

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, UserMixin
+import flask_excel as excel
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,5 +11,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+excel.init_excel(app)
 
 from app import routes, models
