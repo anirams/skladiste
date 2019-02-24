ba9971328da095758c0e7f54ecbd4eb20ea4bb9c
marino2009 <45363000+marino2009@users.noreply.github.com>
2019-02-24 21:19:37 +0100

    jzu

---
+++ migrations/versions/1ead8fe54fa2_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 1ead8fe54fa2
+Revises: bd5054831481
+Create Date: 2019-02-24 21:19:02.652251
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1ead8fe54fa2'
+down_revision = 'bd5054831481'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
