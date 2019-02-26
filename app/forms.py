ac101daa983a4b62d243c78de8d9a2832ce50c4b
aruki87 <ante.rukavina87@gmail.com>
2019-02-26 13:03:17 +0100

    a
    a

--- app/forms.py
+++ app/forms.py
@@ -43,7 +43,7 @@ class UnosProizvodaForm(FlaskForm):
 	zemlja_podrijetla = StringField('Zemlja podrijetla', validators=[DataRequired('Unesi Zemlju Podrijetla')])
 	#oib = IntegerField('OIB', validators=[DataRequired('Unesi OIB Tvrtke (broj)')])
 	#dodaj_jos = SubmitField('Dodaj jos')
-	bar_kod = IntegerField('Bar kod', validators=[DataRequired('Unesi bar kod')])
+	barkod = IntegerField('Bar kod', validators=[DataRequired('Unesi bar kod')])
 	submit2 = SubmitField('Dodaj Proizvod')
 	def validate(self):
 		rv = FlaskForm.validate(self)
