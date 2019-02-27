636d5d02099ee3c06c32a2e03e16ce44bf825f35
marino2009 <45363000+marino2009@users.noreply.github.com>
2019-02-27 22:58:24 +0100

    hjtzjzuk

---
+++ migrations/versions/096513ffa3cc_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 096513ffa3cc
+Revises: 4e9e3cd01ed8
+Create Date: 2019-02-27 22:54:31.409734
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '096513ffa3cc'
+down_revision = '4e9e3cd01ed8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
